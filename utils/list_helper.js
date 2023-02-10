8907bcad8bb6fb33535b11361b71997ee3142cfe
ddoyediran <ddoyediran@gmail.com>
2023-02-10 21:23:12 +0100

    helper functions and unit tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -12,7 +12,45 @@ const totalLikes = (blogPosts) => {
   return totalSum;
 };
 
+// const favoriteBlog = (blogList) => {
+//   let mostLikes = [];
+//   blogList.map((blog) => {
+//     mostLikes.push(blog.likes);
+//   });
+
+//   let blogWithLikes = mostLikes.indexOf(Math.max(...mostLikes));
+
+//   return {
+//     title: blogList[blogWithLikes].title,
+//     author: blogList[blogWithLikes].author,
+//     likes: blogList[blogWithLikes].likes,
+//   };
+// };
+
+const favoriteBlog = (blogList) => {
+  if (blogList.length <= 0) {
+    return "Blog list is empty";
+  }
+
+  let maxLikes = 0;
+  let maxIndex = 0;
+
+  for (let i = 0; i < blogList.length; i++) {
+    if (blogList[i].likes > maxLikes) {
+      maxLikes = blogList[i].likes;
+      maxIndex = i;
+    }
+  }
+
+  return {
+    title: blogList[maxIndex].title,
+    author: blogList[maxIndex].author,
+    likes: blogList[maxIndex].likes,
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
